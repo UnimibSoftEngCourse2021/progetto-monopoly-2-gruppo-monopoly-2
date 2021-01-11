1f54cc8b9b049017c36681f30ec38ae70501c5c3
Alberto Bertagnoli <a.bertagnoli1@campus.unimib.it>
2021-01-11 17:03:38 +0100

    Views index, sistemato Controllerattesa :Alberto

--- Monopoly_Workspace/routes/ControllerInizializzazione.js
+++ Monopoly_Workspace/routes/ControllerInizializzazione.js
@@ -1,23 +1,23 @@
 var express = require('express');
-var Singleton = require('./Model/WebServerSingleton');
+//var Singleton = require('./Model/WebServerSingleton');
 var router = express.Router();
 
 
 
 /* GET home page. */
-router.post('/attesa', function(req, res, next) {
-
-  if(req.body.home_button.equal("Crea Partita")){ // Entra se il bottone ad esser stato schiacciato e "Crea Partita"
+router.post('/', function(req, res, next) {
+  console.log(req.body.home_button);
+  if(req.body.home_button == "cp"){ // Entra se il bottone ad esser stato schiacciato e "Crea Partita"
     var username = req.body.username; //salva in una variabile l'username scelto dal giocatore che ha creato la partita
     // var webServer = Singleton.getWebServerInstance(); //richiama l'istanza della classe WebServer
     // var codPartita = webServer.creaPartita(username); //dall'istanza WebServer richiama la funzione creaPartita
-    res.render('attesa_m', {user : username}) //chiama il render della schermata d'attesa per la sessione TODO inserire
-  }else if (req.body.home_button.equal("Unisciti")){// Entra se il bottone ad esser stato schiacciato e "Unisciti"
+    res.render('attesa_m', {user : username}); //chiama il render della schermata d'attesa per la sessione TODO inserire
+  }else if (req.body.home_button == "uni"){// Entra se il bottone ad esser stato schiacciato e "Unisciti"
     var username = req.body.username; //salva in una variabile l'username dell'utente
-    var codPartita = req.body.codPartita;
+    var codPartita = req.body.code_parita;
     //webServer = Singleton.getWebServerInstance(); //richiama l'istanza della classe WebServer
     //webServer.accediPartita(username, codPartita); //richiama la funzione accedi partita nell'istanza di WebServer
-    res.render('attesa_u', username)
+    res.render('attesa_u', {user: username, cod_p: codPartita});
   }else{
     // res.render( , ) //Render della View di Gioco SchermataDiGioco
   }
